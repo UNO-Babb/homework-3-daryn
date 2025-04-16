a240ea3ecd1374b85b0ccd517baef7e283d544b8
darynjack <darynjackson@unomaha.edu>
2025-04-16 20:00:07 +0000

    hangman

--- BoardGame.py
+++ BoardGame.py
@@ -1,23 +1,121 @@
 #Example Flask App for a hexaganal tile game
 #Logic is in this python file
 
-from flask import Flask, render_template, jsonify
+import random
 
-app = Flask(__name__)
+def choose_word():
+    words = ['snakes', 'games', 'computer', 'winows', 'kansas' , 'dog' , 'lady' , 'girls' , 'starbucks' , 'cupcake' , 'ladder' , 'chair' , 'batman', 'grass']
+    return random.choice(words)
 
-# Keep track of the current player
-current_player = 1
+def display_hangman(tries):
+    stages = [
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   / \\
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   /
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |    |
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |
+           |
+           |
+           |
+        """,
+    ]
+    return stages[tries]
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def play_hangman():
+    word = choose_word()
+    guessed = ["_"] * len(word)
+    tries = 6
+    guessed_letters = []
 
-@app.route('/add_tile')
-def add_tile():
-    global current_player
-    color = 'green' if current_player == 1 else 'blue'  # Alternate between green and blue
-    current_player = 2 if current_player == 1 else 1  # Switch to the other player
-    return jsonify(color=color)
+    print("Welcome to Hangman!")
+    print(display_hangman(tries))
+    print("Word: " + " ".join(guessed))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    while tries > 0 and "_" in guessed:
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input. Please guess a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You've already guessed that letter!")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess in word:
+            print("Correct guess!")
+            for i in range(len(word)):
+                if word[i] == guess:
+                    guessed[i] = guess
+        else:
+            print("Wrong guess!")
+            tries -= 1
+
+        print(display_hangman(tries))
+        print("Word: " + " ".join(guessed))
+
+    if "_" not in guessed:
+        print("Congratulations! You've guessed the word:", word)
+    else:
+        print("Game over! The word was:", word)
+
+def main():
+    while True:
+        play_hangman()
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing! Goodbye!")
+            break
+
+main()
+
+play_hangman()
